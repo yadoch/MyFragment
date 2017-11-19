93398a935f99f61f789c7469d51b9ea4e248736c
yadoch <yadoch@gmail.com>
2017-11-19 09:22:59 +0800

    完成範例 補充說明

--- app/src/main/java/tw/com/abc/myfragment/F2.java
+++ app/src/main/java/tw/com/abc/myfragment/F2.java
@@ -4,23 +4,48 @@ package tw.com.abc.myfragment;
 import android.app.Fragment;
 import android.os.Bundle;
 
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class F2 extends Fragment {
-
-
+    private View view;
+    private MainActivity activity;
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity=(MainActivity) getActivity();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f2, container, false);
+        if (view == null) {
+            //把原本的 return 用 view 取代
+            view = inflater.inflate(R.layout.fragment_f2, container, false);
+            Button test2 = (Button) view.findViewById(R.id.test2);
+            test2.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.i("geoff", "F2 Btn OK!!");
+                    showMain();
+                }
+            });
+        }
+        return view;
     }
+    private void showMain(){
+        int rand= (int)( Math.random() *49+1);
+        // 呼叫 MainActivity 中的setTextView()
+        activity.setTextView("" + rand);
 
+    }
 }
--- app/src/main/java/tw/com/abc/myfragment/MainActivity.java
+++ app/src/main/java/tw/com/abc/myfragment/MainActivity.java
@@ -4,18 +4,25 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private F1 f1;
     private F2 f2;
     private FragmentManager fmgr;
     private FragmentTransaction tran;
+    private boolean isF1;
+    private String TAG ="geoff";
+    private TextView mesg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.activity_main);
+        mesg = (TextView) findViewById(R.id.mesg);
 
         f1 = new F1(); f2 = new F2();
         fmgr = getFragmentManager();
@@ -24,6 +31,32 @@ public class MainActivity extends AppCompatActivity {
         tran = fmgr.beginTransaction();
         tran.add(R.id.container, f1);
         tran.commit();
+        isF1 = true;
+    }
 
+    public void test(View view) {
+        // 透過Booline 特性做True 和 False 轉換,不用再加判斷式
+        isF1 = !isF1;
+        tran =fmgr.beginTransaction();
+        // 用add 會Fragment 會重疊,必須改用 replace
+        //tran.add(R.id.container,f2);
+        // 只能切換f2 用三元運算子再改寫
+        //tran.replace(R.id.container,f2);
+
+        //最終版
+        tran.replace(R.id.container, isF1?f1:f2);
+
+    // 上課應有提到,不知用途,待確認
+    //    tran.addToBackStack(null);
+        tran.commit();
+    }
+
+    public void test1(View view){
+        // 這個 不會有作用,純粹只是驗證test1 按下之後只有Fragment F1 Listen 該事件而非 MainActivity
+        Log.i(TAG, "test1: Main");
+    }
+
+    public void  setTextView(String newmesg){
+        mesg.setText(newmesg);
     }
 }
